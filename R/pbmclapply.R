8cee62bd674b157aaf20826ac5527fdc0a676e05
Kevin Kuang <kvnkuang@users.noreply.github.com>
2016-07-04 12:17:17 -0400

    Added support for Windows

--- R/pbmclapply.R
+++ R/pbmclapply.R
@@ -1,47 +1,56 @@
 library(parallel)
 
-pbmclapply <- function(X, FUN, ...,
-                       mc.preschedule = TRUE, mc.set.seed = TRUE,
-                       mc.silent = FALSE, mc.cores = getOption("mc.cores", 2L),
-                       mc.cleanup = TRUE, mc.allow.recursive = TRUE,
-                       mc.progress=TRUE, mc.style=3)
+pbmclapply <- function(X,
+                       FUN,
+                       ...,
+                       mc.preschedule = TRUE,
+                       mc.set.seed = TRUE,
+                       mc.silent = FALSE,
+                       mc.cores = getOption("mc.cores", 2L),
+                       mc.cleanup = TRUE,
+                       mc.allow.recursive = TRUE,
+                       mc.progress = TRUE,
+                       mc.style = 3)
 {
-  if (!is.vector(X) || is.object(X)) X <- as.list(X)
+  if (!is.vector(X) || is.object(X))
+    X <- as.list(X)
 
   if (mc.progress) {
-    f <- fifo(tempfile(), open="w+b", blocking=T)
-    p <- parallel::makeForkCluster(nnodes = 1)
-    pb <- txtProgressBar(0, length(X), style=mc.style)
-    setTxtProgressBar(pb, 0)
-    progress <- 0
-    if (inherits(p, "masterProcess")) {
-      while (progress < length(X)) {
-        readBin(f, "double")
-        progress <- progress + 1
-        setTxtProgressBar(pb, progress)
-      }
-      cat("\n")
-      parallel::stopCluster()
-    }
+    cl <- makeCluster(1, outfile = "", useXDR = T)
+    clusterCall(cl, function(length) {
+      progress <<- 0
+      pb <<- txtProgressBar(0, length, style = mc.style)
+      setTxtProgressBar(pb, 0)
+      return(0)
+    }, length(X))
   }
 
   tryCatch({
     result <- mclapply(X, function(...) {
       res <- FUN(...)
       if (mc.progress) {
-        writeBin(1, f)
-        }
-      res
+        clusterCall(cl, function() {
+          progress <<- progress + 1
+          setTxtProgressBar(pb, progress)
+          return(0)
+        })
+      }
+      return(res)
     }, ...,
     mc.preschedule = mc.preschedule, mc.set.seed = mc.set.seed,
     mc.silent = mc.silent, mc.cores = mc.cores,
-    mc.cleanup = mc.cleanup, mc.allow.recursive = mc.allow.recursive
-    )
+    mc.cleanup = mc.cleanup, mc.allow.recursive = mc.allow.recursive)
   }, finally = {
     if (mc.progress) {
-      close(f)
+      stopCluster(cl)
     }
   })
 
   return(result)
+}
+
+lazySqrt <- function(num) {
+  # Sleep randomly between 0 to 1 second
+  Sys.sleep(runif(1))
+  return(sqrt(num))
 }
--- R/pbmcmapply.R
+++ R/pbmcmapply.R
@@ -1,13 +1,17 @@
 library(parallel)
 
-pbmcmapply <- function(FUN, ..., MoreArgs = NULL,
-                       mc.preschedule = TRUE, mc.set.seed = TRUE,
-                       mc.silent = FALSE, mc.cores = getOption("mc.cores", 2L),
-                       mc.cleanup = TRUE, mc.progress=TRUE, mc.style=3)
+pbmcmapply <- function(FUN,
+                       ...,
+                       MoreArgs = NULL,
+                       mc.preschedule = TRUE,
+                       mc.set.seed = TRUE,
+                       mc.silent = FALSE,
+                       mc.cores = getOption("mc.cores", 2L),
+                       mc.cleanup = TRUE,
+                       mc.progress = TRUE,
+                       mc.style = 3)
 {
   if (mc.progress) {
-    f <- fifo(tempfile(), open="w+b", blocking=T)
-    p <- parallel::makeForkCluster(nnodes = 1)
     length <- max(mapply(function(element) {
       if (is.null(nrow(element))) {
         return(length(element))
@@ -15,34 +19,39 @@ pbmcmapply <- function(FUN, ..., MoreArgs = NULL,
         return(nrow(element))
       }
     }, list(...)))
-    pb <- txtProgressBar(0, length, style=mc.style)
-    setTxtProgressBar(pb, 0)
-    progress <- 0
-    if (inherits(p, "masterProcess")) {
-      while (progress < length) {
-        readBin(f, "double")
-        progress <- progress + 1
-        setTxtProgressBar(pb, progress)
-      }
-      cat("\n")
-      parallel::stopCluster()
-    }
+    cl <- makeCluster(1, outfile = "", useXDR = T)
+    clusterCall(cl, function(length) {
+      progress <<- 0
+      pb <<- txtProgressBar(0, length, style = mc.style)
+      setTxtProgressBar(pb, 0)
+      return(0)
+    }, length)
   }
 
   tryCatch({
-    result <- mcmapply(function(...) {
-      res <- FUN(...)
-      if (mc.progress) {
-        writeBin(1, f)
-      }
-      res
-    }, ..., MoreArgs = MoreArgs,
-    mc.preschedule = mc.preschedule, mc.set.seed = mc.set.seed,
-    mc.silent = mc.silent, mc.cores = mc.cores, mc.cleanup = mc.cleanup
+    result <- mcmapply(
+      function(...) {
+        res <- FUN(...)
+        if (mc.progress) {
+          clusterCall(cl, function() {
+            progress <<- progress + 1
+            setTxtProgressBar(pb, progress)
+            return(0)
+          })
+        }
+        return(res)
+      },
+      ...,
+      MoreArgs = MoreArgs,
+      mc.preschedule = mc.preschedule,
+      mc.set.seed = mc.set.seed,
+      mc.silent = mc.silent,
+      mc.cores = mc.cores,
+      mc.cleanup = mc.cleanup
     )
   }, finally = {
     if (mc.progress) {
-      close(f)
+      stopCluster(cl)
     }
   })
 
