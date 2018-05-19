ccb74face718d6ff49d6d2660d7a1de1d3cb9ca0
Kevin Kuang <kvnkuang@users.noreply.github.com>
2018-05-18 21:47:40 -0400

    Added R docs and updated test file.

--- test/test.R
+++ test/test.R
@@ -8,7 +8,7 @@ testErrorHandling <- function(num) {
   # Sleep randomly between 0 to 0.5 second
   Sys.sleep(runif(1, 0, 0.5))
   if (num == 2) {
-    stop("aaa")
+    stop("throw error on purpose")
   }
   return(sqrt(num))
 }
@@ -18,15 +18,19 @@ control <- mclapply(1:20, testMultipleArguments, "alice", "bob", 42, "chris")
 # Test four arguments
 res1 <- pbmclapply(1:20, testMultipleArguments, "alice", "bob", 42, "chris")
 # Test four arguments plus parameters
-res2 <- pbmclapply(1:20, testMultipleArguments, "alice", "bob", 42, "chris", mc.cores = 4, mc.style = 2)
+res2 <- pbmclapply(1:20, testMultipleArguments, "alice", "bob", 42, "chris", mc.cores = 4)
 # Check if the results are the same as control
-print(all(identical(control, res1), identical(control, res2)))
+if(!all(identical(control, res1), identical(control, res2))) {
+  warning("Tests for pbmclapply failed.")
+}
 
 # Tests for pbmcmapply
 control <- mcmapply(testMultipleArguments, 1:20, MoreArgs = list("alice", "bob", 42, "chris"))
 # Test four arguments
 res3 <- pbmcmapply(testMultipleArguments, 1:20, MoreArgs = list("alice", "bob", 42, "chris"))
 # Test four arguments plus parameters
-res4 <- pbmcmapply(testMultipleArguments, 1:20, MoreArgs = list("alice", "bob", 42, "chris"), mc.cores = 4, mc.style = 2)
+res4 <- pbmcmapply(testMultipleArguments, 1:20, MoreArgs = list("alice", "bob", 42, "chris"), mc.cores = 4)
 # Check if the results are the same as control
-print(all(identical(control, res3), identical(control, res4)))
+if(!all(identical(control, res3), identical(control, res4))) {
+  warning("Tests for pbmcmapply failed.")
+}
