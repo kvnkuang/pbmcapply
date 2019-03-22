9f40fbe761d662e2df407a7a1a9c723ba878f66b
Kevin Kuang <kvnkuang@users.noreply.github.com>
2019-03-22 13:52:55 -0400

    Fix C compilation on platforms.
    - Linux: disabled remapping and use internal API directly for better stability.
    
    - Windows: fix WIN32 constant.

--- src/process.c
+++ src/process.c
@@ -1,28 +1,34 @@
+#define R_NO_REMAP
 #include <R.h>
 #include <Rinternals.h>
-#ifndef Win32
+
+#ifndef WIN32
 # include <unistd.h>
 # include <signal.h>
 #endif
 
 SEXP setpgid_(SEXP x_) {
-  int id = asInteger(x_);
   int res = 0;
-#ifndef Win32
-  if (setpgid(id, id) == 1) res = 1;
-#else
-  warning(_("set group process id is not supported on this platform"));
-#endif
-  return ScalarLogical(res);
+
+  #ifndef WIN32
+    int id = Rf_asInteger(x_);
+    if (setpgid(id, id) == 1) res = 1;
+  #else
+    Rf_warning("set group process id is not supported on this platform");
+  #endif
+
+  return Rf_ScalarLogical(res);
 }
 
 SEXP killp_(SEXP pgid_) {
-  int id = asInteger(pgid_);
   int res = 0;
-#ifndef Win32
-  if (killpg(id, SIGTERM) == 0) res = 1;
-#else
-  warning(_("kill process is not supported on this platform"));
-#endif
-  return ScalarLogical(res);
+
+  #ifndef WIN32
+    int id = Rf_asInteger(pgid_);
+    if (killpg(id, SIGTERM) == 0) res = 1;
+  #else
+    Rf_warning("kill process is not supported on this platform");
+  #endif
+
+  return Rf_ScalarLogical(res);
 }
